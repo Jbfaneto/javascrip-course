688d8c5769e3e5eef182e22baf1ee22dccda38d9
João Neto <jbfaneto@gmail.com>
2022-08-22 15:31:31 -0300

    done

---
+++ exercicios/Aula11/ex001.js
@@ -0,0 +1,7 @@
+var vel= 60
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel<=50){
+    console.log('dirija sempre com cinto de seguraça!')}
+else{
+    console.log(`Você foi multado, sua velocidade é de ${vel}km/h`)
+}
